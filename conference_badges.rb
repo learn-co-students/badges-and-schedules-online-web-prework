b952dbaaa246d6c2591f5d290ce12efa5df4d7f8
kodyclemens <kody.clemens@gmail.com>
2018-09-30 10:45:10 -0400

    Updated solution using map and each_with_index

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,19 +3,15 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(speakers_name_array)
-  all_speakers = []
-  for name in speakers_name_array do
-    all_speakers.push(badge_maker(name))
+  speakers_name_array.map do |name|
+    badge_maker(name)
   end
-  all_speakers
 end
 
 def assign_rooms(attendees)
-  room_assignments = []
-  attendees.each do |name|
-    room_assignments.push("Hello, #{name}! You'll be assigned to room " + (attendees.index(name) + 1).to_s + "!")
+  attendees.each_with_index.map do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index+1}!"
   end
-  room_assignments
 end
 
 def printer(attendees)
