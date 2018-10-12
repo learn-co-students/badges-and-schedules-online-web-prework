c7ab5a06ad5b7ac6826ad2f77afe3cbd01622976
grant smith <grantsmith1256@gmail.com>
2018-10-12 03:37:31 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def badge_maker(name)
+  name = "Arel"
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map {|attendees| "Hello, my name is #{attendees}."}
+end
+
+def assign_rooms(attendees)
+  attendees.each_with_index(1).map { |attendees, number| "Hello, #{attendees}! You'll be assigned to room #{number}!"}
+end
