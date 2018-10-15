1d27fcbbf7e70865a41188742050f57844271f0d
VICTORIA MANUEL <victoriamanuel.dev@gmail.com>
2018-10-15 16:51:01 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}"
+end
+
+def batch_badge_creator(names)
+  badges = []
+  name.each do |name|
+    badges.push("Hello, my name is #{name}.")
+end
+  badges
+end
+
+
+def assign_rooms() 
+  return "Hello, #{}! You'll be assigned to room #{}!"
+end
