0dfc525c6e4134c80af284ff9a024ecdfba5b80a
Elyse Burmester <burmesterelyse@gmail.com>
2018-10-16 14:02:08 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+require 'enumerator'
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.map do |name|
+    badge_maker(name)
+  end
+end
+
+def assign_rooms(array)
+  array.enum_for(:each_with_index).collect do |name, index| 
+    "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+end
+
+def printer(array)
+  batch_badge_creator(array).each do |badge|
+    puts badge
+  end
+   assign_rooms(array).each do |assignment|
+    puts assignment
+  end
+end
