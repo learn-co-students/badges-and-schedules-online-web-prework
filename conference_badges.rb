e230de3291900857609d1d5b036085c3fc5cc635
Johnathon Wood <jaywood128@gmail.com>
2018-10-19 21:15:26 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+   "Hello, my name is #{name}."
+end
+def batch_badge_creator(array)
+  array.collect do |name|
+    badge_maker(name)
+   end
+end
+def assign_rooms(array)
+  array.map.with_index do |name, i|
+    "Hello, #{name}! You'll be assigned to room #{i+1}!"
+  end
+end
