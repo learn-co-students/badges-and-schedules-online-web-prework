e8a1a6a00aba04d1f20c0ea806c7ce5b12bd239a
Nick Inman <nick@theoatmeal.com>
2018-11-17 18:57:29 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+
+names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  badge =  "Hello, my name is #{name}."
+  return badge
+end
+
+def batch_badge_creator(names)
+  names.map { |x| + "Hello, my name is #{x}."}
+end
+
+def assign_rooms(names)
+  names.each_with_index.map { |x, i| + "Hello, #{x}! You'll be assigned to room #{i + 1}!"}
+end
+
+def printer(names)
+  batch_badge_creator(names).each { |list| puts list }
+  
+  assign_rooms(names).each { |rooms| puts rooms}
+end
