1f5721345fa8d88fc5950467c38347ce777f6188
Robert McCary <robert.mccary@yahoo.com>
2018-11-29 14:52:51 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,8 +4,7 @@ end
 
 def batch_badge_creator(attendees)
   attendees = ["Edsgar", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  attendees.each do |name|
-    return "Hello, my name is #{name}."
+  attendees.each do {|name| puts "Hello, my name is " + name}
+
   end
-end
 end
