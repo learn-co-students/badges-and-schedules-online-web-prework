35760ef46955746e94e48c81a28fcbe6efe3255e
Joe Lott <raymond.j.lott@gmail.com>
2018-12-03 04:04:00 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,14 +11,9 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-
-  room_assignments = []
-
-  attendees.each_with_index do |attendee, index|
-    room_assignments.push("Hello, #{attendee}! You'll be assigned to room #{index + 1}!")
+  attendees.each_with_index.map do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index + 1}!"
   end
-
-  return room_assignments
 end
 
 def printer(attendees)
