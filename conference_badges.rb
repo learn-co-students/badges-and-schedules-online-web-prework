a42e6fb05b55c134537a56fafa9b38a83264a860
Dylan Michael Labat <dml252@live.com>
2019-01-03 01:51:32 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+  speakers.collect do |name|
+    badge_maker(name)
+  end
+end
+
+def assign_rooms(speakers)
+  speakers.collect.each_with_index do |name, room|
+    "Hello, #{name}! You'll be assigned to room #{room + 1}!"
+  end
+end
+
+def printer(speakers)
+  batch_badge_creator(speakers).each do |list|
+    puts list
+  end
+  assign_rooms(speakers).each do |list|
+    puts list
+  end
+end
