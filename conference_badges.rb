431ac233af2d3bff5d8f5319a6d8c2cae47abaca
Kelsey Low <kklowlow@gmail.com>
2019-01-04 03:26:02 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  new_badge = []
+  names.each do |name|
+    new_badge << badge_maker(name)
+  end
+  return new_badge
+end
+
+def assign_rooms(attendees)
+  room_assign = []
+  counter = 1 
+  attendees.each do |attendee|
+    room_assign << "Hello, #{attendee}! You'll be assigned to room #{counter}!"
+    counter += 1 
+  end
+  return room_assign
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |result|
+    puts result
+  end
+  assign_rooms(attendees).each do |result|
+  puts result
+  end
+end
