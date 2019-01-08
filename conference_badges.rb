5dc882058abf17786159ccccf041d5a59b02bf99
Michael J. Schiumo <mschiumo18@gmail.com>
2019-01-08 21:40:29 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def badge_maker(array)
+  "Hello, my name is #{name}."
+ end 
+  
+def batch_badge_creator(attendees)  
+ attendees.map { |attendees| "Hello, my name is #{attendees}."}
+end
+
+  
