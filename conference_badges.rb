c2716481537a9e4daec4b5fcdb8a0fb1b99e05b6
Eyal Toledano <eyaltoledano@me.com>
2019-01-10 23:30:23 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,18 @@ def batch_badge_creator(name_array)
   }
   new_array
 end
+
+def assign_rooms(name_array)
+  new_array = []
+  name_array.each.with_index { |name, room|
+    new_array << "Hello, " + name + "! You'll be assigned to room " + (room + 1).to_s + "!"
+  }
+  new_array
+end
+
+def printer(name_array)
+  badge = batch_badge_creator(name_array)
+  badge.each {|b| puts b }
+  rooms = assign_rooms(name_array)
+  rooms.each {|r| puts r }
+end
