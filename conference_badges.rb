d26bfece31a2872bbb400e5e72ca09f71b4d9e96
Nissan Holzer <nissan.holzer@gmail.com>
2019-01-11 01:12:16 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def badge_maker (name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (names)
+  names.map do |name|
+     badge_maker (name)
+  end
+end 
+
+def assign_rooms (attendees) 
+  attendees.map.with_index(1) do |name, index|
+  "Hello, #{name}! You'll be assigned to room #{index}!"
+  end
+end
+
+def printer (names)
+  badges = batch_badge_creator (names)
+  badges.map do |x| 
+    puts "#{x}"
+  end
+  
+  room_assignment = assign_rooms (names)
+  room_assignment.map do |y|
+    puts "#{y}"
+  end
+end
