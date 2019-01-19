f58412b828ea0e0da15c64a5c182054ab78f98f5
Lu Daley <daleyluis@gmail.com>
2019-01-19 22:37:12 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+ attendees.map do |attendee|
+  "Hello, my name is #{attendee}."
+end
+end 
+
+
+=begin
+
+def batch_badge_creator(attendees)
+  attendees.map do |attendee|
+    "Hello, my name is #{attendee}."
+  end
+end
+
+
+
+#{|e| puts "Hello, my name is {e}" }
+
+fruits.each do |fruit|
+  puts "A fruit of type: #{fruit}"
+=end
+
+  # batch_badge_creator(array) 
+#  returns array
+#end    
