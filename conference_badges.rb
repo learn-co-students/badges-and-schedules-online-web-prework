9c0db043900657eb45ca7b735f5b86753fbd811f
Lu Daley <daleyluis@gmail.com>
2019-01-23 03:48:24 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,14 +10,23 @@ def batch_badge_creator(attendees)
 end
 end 
 
-def assign_rooms
-attendees.map do |attendee|
-attendees.map do |room_assignments|
-"Hello, #{attendee}! You'll be assigned to #{room_assignments}!"
-end
+def assign_rooms(attendee)
+#room_assignments = 1...7
+attendee.each do |cycle|
+#room_assignments.each do |number|
+  puts "Hello, #{attendee}! You'll be assigned to room #{number}!"
+#room_assignments += 1
+#end
 end 
 end
+
 =begin
+
+each_with_index do |item, index|
+puts " Hi #{item}! You are assigned to room #{room}"
+room +=1
+end
+
 
 def batch_badge_creator(attendees)
   attendees.map do |attendee|
@@ -31,8 +40,10 @@ end
 
 fruits.each do |fruit|
   puts "A fruit of type: #{fruit}"
-=end
+end
   # batch_badge_creator(array) 
 #  returns array
 
 #end    
+
+=end
