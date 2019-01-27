0aa34ee3b7f50e1c3307475f761e352f25d5adf1
Ryan Wolfe <ryan@studiohopfitness.com>
2019-01-27 21:58:13 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  list = []
+  list_size = attendees.size
+  list_position = 0
+  while list_position < list_size do
+    list << "Hello, my name is #{attendees[list_position]}."
+    list_position += 1
+  end
+  list
+end
+
+def assign_rooms(attendees)
+  list = []
+  list_size = attendees.size
+  list_position = 0
+  room_number = 1
+  while list_position < list_size do
+    list << "Hello, #{attendees[list_position]}! You'll be assigned to room #{room_number}!"
+    list_position += 1
+    room_number += 1
+  end
+  list
+end
+
+def printer(attendees)
+batch_badge_creator(attendees).each { |x| puts x }
+assign_rooms(attendees).each { |x| puts x }
+end
