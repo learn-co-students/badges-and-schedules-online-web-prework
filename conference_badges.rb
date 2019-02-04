c336f828d27a18dc6f7aa9a93c084bcb1296e951
Chance Smith <chancesmith97@hotmail.com>
2019-02-04 02:18:52 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name)
+  name.collect {|x| badge_maker(x)}
+  end
+
+def assign_rooms(name)
+  name.collect.with_index {|x, i|  "Hello, #{x}! You'll be assigned to room #{i+1}!"}
+  end
+
+def printer(name)
+  batch_badge_creator(name).each {|x| puts x}
+  assign_rooms(name).each {|x| puts x}
+end
