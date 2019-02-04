3dcc326b4d334e48469408c7a5f3be90e96a0897
Brandi Smith <brandi@Brandis-MacBook-Pro.local>
2019-02-04 14:58:18 -0500

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+# Write your code here. 
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_list)
+    message = []
+    for name in name_list do
+        message << badge_maker(name)
+    end
+    message
+end
+
+def assign_rooms(speakers)
+    assignments = []
+    speakers.each_with_index do |speaker, room|
+        assignments << "Hello, #{speaker}! You'll be assigned to room #{room + 1}!"
+    end
+    assignments
+end
+
+def printer(speakers)
+    batch_badge_creator(speakers).each do |speaker|
+        puts speaker
+    end
+
+    assign_rooms(speakers).each do |speaker|
+        puts speaker
+    end
+end
+
