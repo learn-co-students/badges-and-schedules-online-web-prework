78e8b3ba988ce3e21d7359b38df7e8fbc3f03d87
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-11 19:13:30 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,6 +7,13 @@ def batch_badge_creator(attendees)
   attendees.map { |i| badge_maker(i)}
 end
 
-def assign_rooms(room_assignments)
+def assign_rooms(array)
+  room_array = Array.new
+  array.each_with_index {|name, index| room_array << "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+  room_array
 end
-  
+
+def printer(array)
+  batch_badge_creator(array).each {|i| puts i}
+  assign_rooms(array).each {|i| puts i}
+end
