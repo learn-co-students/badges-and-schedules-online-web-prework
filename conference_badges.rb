248e382077177734cfe6a0ef6adab19a5a47c6ed
Kat B <katreesebarnes@gmail.com>
2019-02-11 01:56:58 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -31,8 +31,13 @@ def assign_rooms(attendees)
   welcome_room
   
 end
-
-def printer(attendees) 
+def printer(attendees)
   
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  assign_rooms(attendees).each do |room|
+    puts room
+end
   
 end
