51b01064068db47b01f6de18aa40c4d1c941b440
Samuel W Tong <ssamt1142@gmail.com>
2019-02-23 23:07:49 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+require "pry"
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def batch_badge_creator(speakers)
+  speaker_badge = []
+  speakers.each do |n| 
+    speaker_badge << badge_maker(n)
+  end
+speaker_badge
+end
+
+def assign_rooms(speakers)
+  rooms_assigned = []
+  speakers.each_with_index do |n, r|
+    rooms_assigned << "Hello, #{n}! You'll be assigned to room #{r+1}!"
+  end
+  rooms_assigned
+end
+
+def printer(speakers)
+  batch_badge_creator(speakers).each {|n| puts n}
+  assign_rooms(speakers).each {|n| puts n}
+end
