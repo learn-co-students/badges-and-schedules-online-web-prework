2c4bcff12038be6903ef7e61ffc4d2c3ca1dd071
Charlotte Neff <charlotte.neff@gmail.com>
2019-02-24 02:35:47 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
+def badge_maker(name)
+  puts "Hello,my name is #{name}."
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.each |name|
+  puts "Hello, my name is #{name}"
+end
 # Write your code here.
