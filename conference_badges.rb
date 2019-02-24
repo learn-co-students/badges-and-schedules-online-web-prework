0fbbba111aff883cad8c83dc298afbcd6549f6c6
Charlotte Neff <charlotte.neff@gmail.com>
2019-02-24 03:08:43 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,11 +7,12 @@ end
 
 def batch_badge_creator(attendees)
   attendees.map do |attendee|
-  puts "Hello, my name is #{name}"
+    puts "Hello, my name is #{attendee}."
+  end
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index.map do (attendee, index)
+  attendees.each_with_index.map do |attendee, index|
     "Hello, #{attendee}! You'll be assigned to room #{index+1}"
   end
 end
@@ -20,11 +21,9 @@ def printer(attendees)
   batch_badge_creator(attandees).each do |badge|
     puts badge
   end
+  
   assign_rooms(attendees).each do |assignment|
     puts assignment
   end
+  
 end
-
-
-
-# Write your code here.
