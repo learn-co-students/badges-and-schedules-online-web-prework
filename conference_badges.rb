f4efd9d2411deadc1ab6aa0f57a26f76a3a1dc65
Charlotte Neff <charlotte.neff@gmail.com>
2019-02-24 03:11:04 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,7 @@
-attendees = ["Edsgar", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
   puts "Hello,my name is #{name}."
-  return "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(attendees)
