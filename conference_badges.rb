07c67ca79f086f3a02f84a265496737367c742d8
David Thayer <dthayer16@gmail.com>
2019-03-01 21:09:03 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (names)
+  array = []
+  names.each do |x|
+    array << badge_maker(x)
+  end
+  array
+end
+
+def assign_rooms (speakers)
+  array = []
+  speakers.each_with_index do {|item, index| speakers[item] = index}
+  array << badge_maker(item, index)
+end
+  array
+end
+
+def printer
+
+end
