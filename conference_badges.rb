2cf87ba1ac7d380f76c8d186a655e487374658ce
Charlotte Goldthwaite <chargold201@gmail.com>
2019-03-05 01:20:43 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,10 +12,6 @@ def assign_rooms(speakers)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees).each do |badge|
-    puts badge
-  end
-  assign_rooms(attendees).each do |room_assignment|
-    puts room_assignment
-  end
+  batch_badge_creator(attendees).each {|badge| puts badge}
+  assign_rooms(attendees).each {|room_assignment| puts room_assignment}
 end
