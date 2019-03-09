2b8127ca15e7c8d39757161445629dfcf7a6a6c7
Patrick A <akolo@hotmail.com>
2019-03-09 18:28:48 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+
+#def #conference_badges 
+
+
+#def conference_badges ( name, attendees, badges, room_assignments, badges_and_room_assignments)
+  
+def #badge_maker(attendees)
+  attendees.each {" Hello, my name is" |x| print x}
+End   
