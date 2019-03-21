06efac90a21b2d19f2490825ecdac34bb68da3e3
Michael Chen <michaelchengaming@gmail.com>
2019-03-21 05:23:50 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,13 +5,13 @@ end
 
 def batch_badge_creator(attendees)
   attendees.map {|name| "Hello, my name is #{name}."}
-end
+end # use .map to create a new array and operate on each member of that collection
 
 def assign_rooms(attendees)
   attendees.each_with_index.map {|name, room| "Hello, #{name}! You'll be assigned to room #{room += 1}!"}
-end
+end # attendees.map.with_index works the same {|item, index| "#{item}, #{index += 1} to increment each time"}
 
 def printer(attendees)
   batch_badge_creator(attendees).each {|names| puts names}
   assign_rooms(attendees).each {|rooms| puts rooms}
-end
+end # .each to iterate over each item in array and puts to print them
