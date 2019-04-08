2fc9c56c89001ec722e61e50f314f262e8649a40
Erin Ponsonby <erinponsonby@gmail.com>
2019-04-08 16:31:57 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,11 +17,15 @@ end
 def batch_badge_creator(attendees)
   badge_created = 0
   final_array = []
+  # creates an empty array
   attendees.each do |speaker|
+    #iterate over attendees array
     final_array.push("Hello, my name is #{speaker}.")
+    # for each attendee, push string onto final array
     badge_created += 1
   end
   return final_array
+  # return an array of badges
 end
 
 def assign_rooms(attendees)
@@ -32,6 +36,7 @@ def assign_rooms(attendees)
       room_number += 1
     end
     return final_room_number_array
+    # returns a list of welcome messages and room assignments
 end
 
 def printer(attendees)
@@ -39,7 +44,9 @@ def printer(attendees)
   name_element_number = 0
   array_of_badges = batch_badge_creator(attendees)
   array_of_badges.each do |greeting|
+    # for each element in the array of badges
     puts greeting
+    # puts greeting, "Hello my name is name"
     name_element_number += 1
   end
   
