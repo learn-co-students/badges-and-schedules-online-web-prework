645f2aec34f3bc37a8e790e9a54dfab9c096972e
Michael Chen <michaelchengaming@gmail.com>
2019-04-20 15:30:00 -0700

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,17 +1,18 @@
-# Write your code here.
+require "pry"
+
 def badge_maker(name)
-  return "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
-  attendees.map {|name| "Hello, my name is #{name}."}
-end # use .map to create a new array and operate on each member of that collection
+def batch_badge_creator(names)
+  names.map { |name| "Hello, my name is #{name}."}
+end
 
-def assign_rooms(attendees)
-  attendees.each_with_index.map {|name, room| "Hello, #{name}! You'll be assigned to room #{room += 1}!"}
-end # attendees.map.with_index works the same {|item, index| "#{item}, #{index += 1} to increment each time"}
+def assign_rooms(speakers)
+  speakers.map.with_index { |name, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!" }
+end
 
-def printer(attendees)
-  batch_badge_creator(attendees).each {|names| puts names}
-  assign_rooms(attendees).each {|rooms| puts rooms}
-end # .each to iterate over each item in array and puts to print them
+def printer(names)
+  batch_badge_creator(names).each { |name| puts name}
+  assign_rooms(names).each { |name| puts name}
+end
