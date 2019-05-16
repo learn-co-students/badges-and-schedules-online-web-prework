f50367d5c47a5326bb54f2cadab3264e3b0019d0
Jack Atherton <jackrrye@gmail.com>
2019-05-16 23:59:08 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}"
+end
+
+def batch_badge_creator(array)
+  for each do |x|
+    puts badge_maker(array[x])
+  end
+end
+
+def assign_rooms(array)
+  
