c3e03e53341e046ff16bc0062a5276d053dd8893
Jack Atherton <jackrrye@gmail.com>
2019-05-17 19:03:08 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,5 +7,14 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(array)
-  array.each_with_index { |name, index| }
+  array.each_with_index.collect { |name, index| "Hello, #{name}! You'll be assigned to room #{index+1}!"}
+end
+
+def printer(array)
+  batch_badge_creator(array).each do |x|
+    puts x 
+  end
+  assign_rooms(array).each do |x|
+    puts x
+  end
 end
