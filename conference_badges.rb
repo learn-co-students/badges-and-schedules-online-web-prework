476f29b8b4a03e37f77e6fb4fa434f4b38d190a2
Jack Atherton <jackrrye@gmail.com>
2019-05-17 03:43:03 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,8 +3,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-  array.collect {|x| "Hello my name is #{x}"}
+  array.collect {|x| badge_maker(x)}
 end
 
 def assign_rooms(array)
+  array.each_with_index { |name, index| }
 end
