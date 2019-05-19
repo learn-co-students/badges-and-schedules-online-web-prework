279f33f19425809464347296a9c7b75781f681b2
layla dantas <laylatdantas@gmail.com>
2019-05-19 15:17:35 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def badge_maker
+   
+end
+
+
+
+
+def batch_badge_creator(array)
+    attendees = badges 
+    end
+
+  def assign_rooms(array)
+    array = (room_assignments)
+end 
+
+  def printer(array) 
+  batch_badge_creator(array).each do
+    |x| 
+    puts |x|
+    
+    assign_rooms(array).each do 
+      |x|
+      puts |x|
