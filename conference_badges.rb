8ff2127a27ad156afec988a79c4d2245a4eeaeae
Huy Do <huyddo40@gmail.com>
2019-05-22 05:03:56 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,4 @@
-require 'pry'
+# require 'pry'
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
@@ -24,6 +24,11 @@ end
 
 def printer(attendees)
   
-  puts "#{batch_badge_creator(attendees).first}"
-  bing.pry 
+  batch_badge_creator(attendees).each do |badge|
+    puts "#{badge}"
+  end
+  assign_rooms(attendees).each do |room|
+    puts "#{room}"
+  end
+    # binding.pry
 end
