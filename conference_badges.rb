e9fa47bc9331482c30c270752d28075cbf484ccc
witty-banner-1950 <asimsali95@gmail.com>
2019-05-23 18:48:27 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (speakers)
+  new_array = []
+  speakers.each do |name|
+    new_array.push("Hello, my name is #{name}.")
+  end
+  return new_array
+end
+
+def assign_rooms (speakers)
+  new_array = []
+  counter = 1
+  speakers.each do |name|
+    new_array.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  return new_array
+end
+
+def printer (speakers)
+  batch_badge_creator(speakers). each do |badge|
+    puts badge
+  end
+
+  assign_rooms(speakers). each do |assignment|
+    puts assignment
+  end
+end
