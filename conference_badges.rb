04403b3774c6f6050db53465905ced7cd8d798ba
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-05-28 22:37:20 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def badge_maker(attendees)
+   return "Hello, my name is #{attendees}."
+end
+
+def batch_badge_creator(attendees)
+    attendees.map {|name|  badge_maker(name)}
+end
+
+def assign_rooms(attendees)
+   return room_assignments=attendees.map.each_with_index { |e,index|  "Hello, #{e}! You'll be assigned to room #{index+1}!"}
+end
+
+def printer(attendees)
+
+   puts batch_badge_creator(attendees)
+badge_maker.pop(attendees)
+
+end
