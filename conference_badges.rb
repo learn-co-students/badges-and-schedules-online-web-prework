8900e11713c18b3db08c8b95618d68f865e495b1
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-05-29 16:48:06 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,18 +1,22 @@
-def badge_maker(attendees)
-   return "Hello, my name is #{attendees}."
+require "pry"
+def badge_maker(name)
+      "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(attendees)
-    attendees.map {|name|  badge_maker(name)}
+     attendees.map {|name|  badge_maker(name)}
+
 end
 
 def assign_rooms(attendees)
-   return room_assignments=attendees.map.each_with_index { |e,index|  "Hello, #{e}! You'll be assigned to room #{index+1}!"}
+  size=attendees.size
+  count=0
+    room_assignments=attendees.map { |e|  "Hello, #{e}! You'll be assigned to room #{count+=1}!"}
 end
 
 def printer(attendees)
-
-   puts batch_badge_creator(attendees)
-badge_maker.pop(attendees)
-
+  array = batch_badge_creator(attendees)
+  array.each{|badge| puts badge}
+  array2 = assign_rooms(attendees)
+  array2.each{|room| puts room}
 end
