11a23193f47a3639d4dd2d9e2a3b7e30bd25cced
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-29 21:19:11 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakerArr)
+  batch_of_badges = speakerArr.map do |speaker|
+    badge_maker(speaker)
+  end
+end
+
+def assign_rooms(speakerArr)
+  room_assignments = speakerArr.map.with_index do |speaker, index|
+      "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
+  end
+end
+
+def printer(speakerArr)
+  badges = batch_badge_creator(speakerArr)
+  badges.each do |badge|
+    puts badge
+  end
+
+  rooms = assign_rooms(speakerArr)
+  rooms.each do |room|
+    puts room
+  end
+    
+end
