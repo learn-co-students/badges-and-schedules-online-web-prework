0b6d4379f564e2e693ce5babd38ff78cc3e33238
Pwako11 <akolo@hotmail.com>
2021-03-06 12:29:03 -0500

    completed lab

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,32 @@
-# Write your code here.
+require 'pry' 
 
-#def #conference_badges 
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(names)
+  names.map{|name|
+    badge_maker(name)
+  } 
+end 
+
+def assign_rooms(names)
+  new_arry = []
+  names.each_with_index{ |item, index|
+  new_arry << "Hello, #{item}! You'll be assigned to room #{index + 1}!"
+  }
+  new_arry
+end 
 
 
-#def conference_badges ( name, attendees, badges, room_assignments, badges_and_room_assignments)
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end 
   
-def #badge_maker(attendees)
-  attendees.each {" Hello, my name is" |x| print x}
-End   
+  assign_rooms(attendees).each do |name|
+    puts name
+  end 
+
+end 
